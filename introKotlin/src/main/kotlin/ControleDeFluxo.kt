3469b564e90b3b5408a7735e33ebf55e0bd8bf53
Wallace-Winkler <wallace.html66@gmail.com>
2023-03-30 14:18:40 -0400

    Controle de fluxo

--- introKotlin/src/main/kotlin/ControleDeFluxo.kt
+++ introKotlin/src/main/kotlin/ControleDeFluxo.kt
@@ -1,13 +1,30 @@
-fun main(args: List<String>){
+class ControleDeFluxo {
+    fun principal() {
+        cases(1)
+        cases(0L)
+        cases("Hello")
+        cases("hello")
+        cases(MyClass("Wallace", 20, "07/06/2002"))
 
-}
+        println("====================================================")
 
-fun cases(obj: Any) {
-    when (obj) {
-        1 -> println("One")
-        "Hello" -> println("Greeting")
-        is Long -> println("Long")
-        !is String -> println("Not a String")
-        else -> println("Unknown")
+        val cakes = listOf<String>("Carrot", "Chocolate", "Orange", "Pineapple")
+
+        for (cake in cakes) {
+            println("It's a $cake cake")
+        }
+
+    }
+
+    fun cases(obj: Any) {
+        when (obj) {
+            1 -> println("One")
+            "Hello" -> println("Greeting")
+            is Long -> println("Long")
+            !is String -> println("Not a String")
+            else -> println("Unknown")
+        }
     }
 }
+
+class MyClass (nome: String, idade: Int, dataDeNascimento: Any)
--- introKotlin/src/main/kotlin/HelloWorld.kt
+++ introKotlin/src/main/kotlin/HelloWorld.kt
@@ -1,4 +1,6 @@
 // Definição da função main
 fun main() {
     println("Hello World!")
+    var controle = ControleDeFluxo()
+    controle.principal()
 }
