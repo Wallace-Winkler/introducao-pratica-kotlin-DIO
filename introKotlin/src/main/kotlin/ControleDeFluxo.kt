666292c2f04c1487f1f707e46c25d7a5e8edbf94
Wallace-Winkler <wallace.html66@gmail.com>
2023-03-30 13:30:20 -0400

    Controle de fluxo

---
+++ introKotlin/src/main/kotlin/ControleDeFluxo.kt
@@ -0,0 +1,13 @@
+fun main(args: List<String>){
+
+}
+
+fun cases(obj: Any) {
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a String")
+        else -> println("Unknown")
+    }
+}
--- introKotlin/src/main/kotlin/HelloWorld.kt
+++ introKotlin/src/main/kotlin/HelloWorld.kt
@@ -1,5 +1,4 @@
 // Definição da função main
 fun main() {
-    // Chamada do método para imprimir o Hello World!
     println("Hello World!")
 }
